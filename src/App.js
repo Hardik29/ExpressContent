902144880aabcf8e91ed1d5fa26643c063f3d8e2
Hardik singh <singhhardik14@gmail.com>
2022-09-01 04:25:24 +0530

    SOP

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route exact path="/SOP" element={<SOP/>} />
-          <Route exact path="/About" element={<About />} />
-          <Route exact path="/ContactUs" element={<ContactUs />} />
-          <Route exact path="/Sign" element={<Sign />} />
+          <Route path="/SOP" element={<SOP/>} />
+          <Route path="/About" element={<About />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/Sign" element={<Sign />} />
         </Routes>
         <a href="https://github.com/mertJF/tailblocks" class="animate-bounce rounded-full w-16 h-16 bg-gray-100 fixed bottom-0 right-0 flex items-center justify-center text-gray-800 mr-8 mb-8 shadow-lg" target="_blank">
           <img src={whatsapp} class="h-16 w-16" />
--- src/Pages/SOP.js
+++ src/Pages/SOP.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import ServicesSec from '../Components/ServicesSec'
+import S from '../Utilites/S.jpg'
+
+function Card(){
+  return(
+ 
+<div class="max-w-sm rounded overflow-hidden shadow-lg m-2 ">
+  <a href="#">
+  <img class="w-full" src={S} alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">Doctoral Programs SOP</div>
+  </div>
+  </a>
+</div>
+
+  )
+}
 
 function SOP() {
   return (
-    <ServicesSec/>
+    <section class="text-gray-700 body-font border-t border-gray-200">
+    <div class="container py-24 px-5 mx-auto">
+      <div class="flex flex-wrap justify-center items-center">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  </section>
   )
 }
 
